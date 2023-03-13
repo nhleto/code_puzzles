eb49c25d4c63e002cfea68240e63678634166976
Henry <hnr.leto11@gmail.com>
2023-03-13 10:07:57 -0500

    mess around

--- leetcode/twosum.js
+++ leetcode/twosum.js
@@ -3,18 +3,19 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums = [2,7,11,15], target = 9) {
-	let hash = {};
-	
-	for(let i = 0; i < nums.length; i++) {
-		const n = nums[i];
-		if(hash[target - n] !== undefined) {
-			return [hash[target - n], i];
-		}
-		hash[n] = i;
-	}
-	return [];
+var twoSum = function (nums = [2, 7, 11, 15], target = 9) {
+  let result = new Map();
 
+  for (i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (result.has(complement)) {
+      return [i, result.get(complement)];
+    }
+
+    result.set(nums[i], i);
+  }
+
+  return null;
 };
 
-console.log(twoSum())
+console.log(twoSum());
